3ac545986b431b079f93c472223f8fce4d8b32c5
AlaaBenk94 <a.benkarrad@gmail.com>
2019-09-19 16:57:07 +0200

    Create abonnement and desabonnement in serveur class

--- root/emprunt/src/main/java/tiw1/serveur/Serveur.java
+++ root/emprunt/src/main/java/tiw1/serveur/Serveur.java
@@ -1,4 +1,16 @@
 package tiw1.serveur;
 
+import tiw1.emprunt.model.Abonne;
+
 public class Serveur {
+
+    // Gestionnaire des abonnés
+    public boolean abonnement(Abonne abonne) {
+        return false;
+    }
+
+    public boolean desabonnement(Abonne abonne) {
+        return false;
+    }
+
 }
