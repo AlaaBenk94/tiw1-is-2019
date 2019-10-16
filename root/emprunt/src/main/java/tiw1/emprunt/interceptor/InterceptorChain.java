22b43d4e97e8a94ed955f6a359c77fac6d376206
BENKARRAD Alaa Eddine <alaa-eddine.benkarrad@etu.univ-lyon1.fr>
2019-10-17 00:55:13 +0200

    Interceptor Pattern
    * Add interceptor and InterceptorChain interfaces
    * Add InterceptorChain implementation
    * Add LoggerInterceptor to log requests

---
+++ root/emprunt/src/main/java/tiw1/emprunt/interceptor/InterceptorChain.java
@@ -0,0 +1,22 @@
+package tiw1.emprunt.interceptor;
+
+import java.util.List;
+
+public abstract class InterceptorChain implements Interceptor{
+    protected List<Interceptor> interceptorsList;
+
+    public InterceptorChain() {}
+
+    public InterceptorChain(List<Interceptor> interceptorsList) {
+        this.interceptorsList = interceptorsList;
+    }
+
+    public void setInterceptorsList(List<Interceptor> interceptorsList) {
+        this.interceptorsList = interceptorsList;
+    }
+
+    public void addInterceptor(Interceptor interceptor) {
+        interceptorsList.add(interceptor);
+    }
+
+}
---
+++ root/emprunt/src/main/java/tiw1/emprunt/interceptor/InterceptorChainImpl.java
@@ -0,0 +1,34 @@
+package tiw1.emprunt.interceptor;
+
+import java.util.List;
+import java.util.Map;
+
+public class InterceptorChainImpl extends InterceptorChain {
+    public InterceptorChainImpl() {
+    }
+
+    public InterceptorChainImpl(List<Interceptor> interceptorsList) {
+        super(interceptorsList);
+    }
+
+    @Override
+    public Map<String, Object> input(String method, Map<String, Object> params) {
+        Map<String, Object> result = params;
+
+        for(Interceptor i: interceptorsList) {
+            result = i.input(method, result);
+        }
+
+        return result;
+    }
+
+    @Override
+    public Object output(String method, Map<String, Object> params, Object result) {
+
+        for(Interceptor i: interceptorsList) {
+            result = i.output(method, params, result);
+        }
+
+        return result;
+    }
+}
