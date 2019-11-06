ba6c234309714b0e8e9822daa967189b57f72597
BENKARRAD Alaa Eddine <alaa-eddine.benkarrad@etu.univ-lyon1.fr>
2019-11-06 10:11:37 +0100

    Init project

---
+++ root/emprunt/src/test/java/tiw1/Tiw1Tp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tiw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tiw1Tp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
